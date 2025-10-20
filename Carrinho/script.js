6ca202fdd71bd6c5c22755581e5dd8517d0d69be
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-10-20 10:04:13 -0300

    Add files via upload

--- Carrinho/script.js
+++ Carrinho/script.js
@@ -35,3 +35,44 @@ $(document).ready(function() {
 
     exibirCarrinho()
 })
+
+function gerar(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+
+    const listaClone = listaElement.cloneNode(true)
+
+    $(listaClone).find("button").remove()
+
+    const listaHtml = listaClone.innerHTML
+    const totalHtml = totalElement.innerHTML
+
+    const conteudoHTML = `
+        <html>
+            <head>
+                <meta charset="UTF-8">
+            <head>
+            <body>
+                <h1>PEDIDO CONFIRMADO</h1>
+                <h3>Agradecemos sua compra e sua preferência.</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            <body>
+        </html>
+    `
+
+    const blob = new Blob([conteudoHTML], {type: "application/msword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.concluido"
+    link.click()
+    document.getElementById("pedido").style.display = "block"
+}
+
+function successClose(){
+    document.getElementById("pedido").style.display = "none"
+}
